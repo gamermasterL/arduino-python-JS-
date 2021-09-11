11970377faf6351eb2baf77d5891c2485d6a9242
gamermasterL <a01054678017@gmail.com>
2021-09-11 16:19:34 +0900

    노래

--- test.ino
+++ test.ino
@@ -1,32 +1,54 @@
-#include <LiquidCrystal_I2C>
-int trig = 9;
-int echo = 8;
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
-void setup(){
-    lcd.begin(16, 2);
-    Serial.begin(9600);
-    pinMode(trig, OUTPUT);
-    pinMode(echo, INPUT);
 
+int piezoPin = 3;
+int echoPin = 13;
+int trigPin = 12;
+
+void setup() {
+  //모듈 핀모드(pinMode) 선언
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(piezoPin, OUTPUT);
 }
 
-void loop(){
- long duration, distance;
-
-  digitalWrite(trig, LOW);
+void loop() {
+  digitalWrite(trigPin, LOW);
+  digitalWrite(echoPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trig, LOW);
+  digitalWrite(trigPin, LOW);
 
-  duration = pulseIn (echo, HIGH);
-  distance = duration * 17 / 1000; 
+  unsigned long duration = pulseIn(echoPin, HIGH);
 
-  Serial.print(distance);
-  Serial.println(" Cm");
+  float distance = duration / 29.0 / 2.0;
 
-  lcd.setCursor(1, 1);    
-  lcd.print(distance);
-  delay(1000);     
- 
+  
+/*
+ * 측정된 거리값을 사용하여 거리에 맞는 음계 출력(6옥타브 음계 출력)
+ * 50cm 미만일 때 소리가 출력됩니다. 50cm를 초과하면 소리 X
+ * 5cm간격으로 5, 10, 15, 20, 25씩 소리가 출력됩니다.
+ */
+  if (distance < 50) {
+    if (distance <= 5) {
+      tone(piezoPin, 1047);            //도
+    } else if (distance <= 10) {
+      tone(piezoPin, 1174);            //레     
+    } else if (distance <= 15) {
+      tone(piezoPin, 1318);            //미
+    } else if (distance <= 20) {
+      tone(piezoPin, 1396);            //파
+    } else if (distance <= 25) {
+      tone(piezoPin, 1567);            //솔
+    } else if (distance <= 30) {
+      tone(piezoPin, 1760);            //라
+    } else if (distance <= 35) {
+      tone(piezoPin, 1975);            //시
+    } else if (distance <= 40) {
+      tone(piezoPin, 2093);            //도
+    }
+  }
+  else {
+    noTone(piezoPin);
+  }
+  delay(100);
 }

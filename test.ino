86e8bcec556d556e0516f54a5ee3e80e5137ad34
gamermasterL <a01054678017@gmail.com>
2021-09-11 15:59:51 +0900

    아두이노 거리 측정기

--- test.ino
+++ test.ino
@@ -1,7 +1,32 @@
+#include <LiquidCrystal_I2C>
+int trig = 9;
+int echo = 8;
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 void setup(){
+    lcd.begin(16, 2);
+    Serial.begin(9600);
+    pinMode(trig, OUTPUT);
+    pinMode(echo, INPUT);
 
 }
 
 void loop(){
-    
+ long duration, distance;
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  duration = pulseIn (echo, HIGH);
+  distance = duration * 17 / 1000; 
+
+  Serial.print(distance);
+  Serial.println(" Cm");
+
+  lcd.setCursor(1, 1);    
+  lcd.print(distance);
+  delay(1000);     
+ 
 }
